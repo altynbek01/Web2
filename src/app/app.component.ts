6a3400c6b430eba6484ab680628a5045935c765c
Altynbek <altynbek01@gmail.com>
2021-02-24 14:54:42 +0600

    'init'

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  image1: string =
+    "https://m.media-amazon.com/images/I/71XXJC7V8tL._FMwebp__.jpg";
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
---
+++ src/app/products.ts
@@ -0,0 +1,109 @@
+export const products = [
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51-6BAu2kbL._AC_SX522_.jpg",
+    id: 1,
+    name: "Samsung Galaxy S10",
+    price: 799,
+    description:
+      "Samsung Galaxy S10 SM-G973F 128GB 8GB RAM International Version - Prism Silver",
+    rating: "&#9733",
+    link:
+      "https://www.amazon.com/Samsung-Galaxy-Verizon-Unlocked-128GB/dp/B07PB5BXMZ/ref=sr_1_10?dchild=1&keywords=samsung+s10%2B&qid=1614143422&refinements=p_89%3ASamsung+Electronics&rnid=2528832011&s=wireless&sr=1-10"
+  },
+  {
+    image: "https://m.media-amazon.com/images/I/71MHTD3uL4L._FMwebp__.jpg",
+    id: 2,
+    name: "Phone 12",
+    price: 699,
+    description:
+      "New Apple iPhone 12 Pro Max (128GB, Pacific Blue) [Locked] + Carrier Subscription",
+    rating: 5
+  },
+
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51IRteuBUVL._AC_SX522_.jpg",
+    id: 3,
+    name: "OnePlus 6",
+    price: 299,
+    description:
+      "OnePlus 6 A6003 Dual-SIM (128GB Storage | 8GB RAM) (GSM Only, No CDMA) Factory Unlocked 4G Smartphone (Midnight Black) - International Version",
+    rating: 6
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81fstJkUlaL._AC_SY450_.jpg",
+    id: 4,
+    name: "Asus F5",
+    price: 900,
+    description:
+      "ASUS F512JA-AS34 VivoBook 15 Thin and Light Laptop, 15.6” FHD Display, Intel i3-1005G1 CPU, 8GB RAM, 128GB SSD, Backlit Keyboard, Fingerprint, Windows 10 Home in S Mode, Slate Gray",
+    rating: 8
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71q621vaqkL._AC_SX425_.jpg",
+    id: 5,
+    name: "Acer Aspire 3",
+    price: 950,
+    description:
+      "Acer Aspire 3 Intel Core i5-1035G1 8GB 256 GB SSD 15.6-Inch Full HD (1920 x 1080) Win 10 Laptop",
+    rating: 4
+  },
+
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71thf1SYnGL._AC_SX342_.jpg",
+    id: 6,
+    name: "Apple MacBook Air",
+    price: 1000,
+    description:
+      "Apple MacBook Air (13-inch Retina Display, 8GB RAM, 256GB SSD Storage) - Gold (Previous Model)",
+    rating: 5
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/6191vr70ZSL._AC_SY355_.jpg",
+    id: 7,
+    name: "Apple MacBook Pro",
+    price: 1050,
+    description:
+      "Apple MacBook Pro 16-in 2.4GHz 8-core i9 32GB 1TB 5300M 4GB Space Gray (CTO)",
+    rating: 8
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/61m8TiyGBWL._AC_SX679_.jpg",
+    id: 8,
+    name: "Samsung Galaxy Tab S7",
+    price: 500,
+    description: "Samsung Galaxy Tab S7 Wi-Fi, Mystic Silver -128 GB",
+    rating: 6
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/718B6zl%2Bb6L._AC_SX466_.jpg",
+    id: 9,
+    name: "Samsung Galaxy Tab S6",
+    price: 400,
+    description: "Samsung Galaxy Tab S6 Lite 10.4",
+    rating: 3
+  },
+  {
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/815ztYEEwYL._AC_SX342_.jpg",
+    id: 10,
+    name: "Apple iPad Pro",
+    price: 750,
+    description:
+      "2020 Apple iPad Pro (11-inch, Wi-Fi, 256GB) - Space Gray (2nd Generation)",
+    rating: 9
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
